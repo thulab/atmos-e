453300811b27358849d3c3421e1245235327e2af
FLOW4215 <qingxin.feng@hotmail.com>
2023-03-28 12:11:35 +0800

    修改错误的任务表名称

--- script/se_insert.sh
+++ script/se_insert.sh
@@ -24,7 +24,7 @@ USERNAME="iotdbatm"
 PASSWORD="iotdb2019"
 DBNAME="QA_ATM"  #数据库名称
 TABLENAME="test_result_se_insert" #数据库中表的名称
-TASK_TABLENAME="ex_commit_history" #数据库中任务表的名称
+TASK_TABLENAME="commit_history" #数据库中任务表的名称
 ############公用函数##########################
 #echo "Started at: " date -d today +"%Y-%m-%d %H:%M:%S"
 init_items() {
