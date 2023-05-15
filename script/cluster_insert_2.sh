cb6724821f535990500723d0a4d4c1a1afd9cd20
FLOW4215 <qingxin.feng@hotmail.com>
2023-05-15 18:12:39 +0800

    优化监控过程数据采集

--- script/cluster_insert_2.sh
+++ script/cluster_insert_2.sh
@@ -317,8 +317,8 @@ if [ "$check_config_num" == "$config_num" ] && [ "$check_data_num" == "$data_num
 fi
 }
 monitor_test_status() { # 监控测试运行状态，获取最大打开文件数量和最大线程数
-	maxNumofOpenFiles=(0 0 0 0)
-	maxNumofThread=(0 0 0 0)
+	maxNumofOpenFiles=(0 0 0 0 0 0)
+	maxNumofThread=(0 0 0 0 0 0)
 	while true; do
 		for (( j = 1; j <= 5; j++ ))
 		do
@@ -326,10 +326,21 @@ monitor_test_status() { # 监控测试运行状态，获取最大打开文件数
 			temp_thread_num_d=0
 			temp_file_num_c=0
 			temp_thread_num_c=0
-			temp_file_num_d=$(ssh ${ACCOUNT}@${D_IP_list[${j}]} "ps aux | grep -v grep | grep DataNode | awk '{print \$2}' | xargs /usr/sbin/lsof -p | wc -l" 2>/dev/null)
-			temp_thread_num_d=$(ssh ${ACCOUNT}@${D_IP_list[${j}]} "pstree -p \$(ps -e | grep ConfigNode | awk '{print \$1}') | wc -l" 2>/dev/null)
-			temp_file_num_c=$(ssh ${ACCOUNT}@${D_IP_list[${j}]} "ps aux | grep -v grep | grep DataNode | awk '{print \$2}' | xargs /usr/sbin/lsof -p | wc -l" 2>/dev/null)
-			temp_thread_num_c=$(ssh ${ACCOUNT}@${D_IP_list[${j}]} "pstree -p \$(ps -e | grep ConfigNode | awk '{print \$1}') | wc -l" 2>/dev/null)	
+		
+			iotdb_pid=$(jps | grep DataNode | awk '{print $1}')
+			if [ "${iotdb_pid}" = "" ]; then
+				echo "无DataNode程序！"
+			else
+				temp_file_num_d=$(ssh ${ACCOUNT}@${D_IP_list[${j}]} "ps aux | grep -v grep | grep DataNode | awk '{print \$2}' | xargs /usr/sbin/lsof -p | wc -l" 2>/dev/null)
+				temp_file_num_c=$(ssh ${ACCOUNT}@${C_IP_list[${j}]} "ps aux | grep -v grep | grep DataNode | awk '{print \$2}' | xargs /usr/sbin/lsof -p | wc -l" 2>/dev/null)
+			fi
+			iotdb_pid=$(jps | grep ConfigNode | awk '{print $1}')
+			if [ "${iotdb_pid}" = "" ]; then
+				echo "无ConfigNode程序！"
+			else
+				temp_thread_num_d=$(ssh ${ACCOUNT}@${D_IP_list[${j}]} "pstree -p \$(ps -e | grep ConfigNode | awk '{print \$1}') | wc -l" 2>/dev/null)
+				temp_thread_num_c=$(ssh ${ACCOUNT}@${C_IP_list[${j}]} "pstree -p \$(ps -e | grep ConfigNode | awk '{print \$1}') | wc -l" 2>/dev/null)
+			fi				
 			#监控打开文件数量			
 			let temp_file_num=${temp_file_num_d}+${temp_file_num_c}
 			if [ ${maxNumofOpenFiles[${j}]} -lt ${temp_file_num} ]; then
