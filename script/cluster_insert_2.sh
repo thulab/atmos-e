d0a09d89642bd2fdf836acf02220f78bddc3f918
FLOW4215 <qingxin.feng@hotmail.com>
2023-05-15 15:41:42 +0800

    Update cluster_insert_2.sh

--- script/cluster_insert_2.sh
+++ script/cluster_insert_2.sh
@@ -459,16 +459,16 @@ else
 	#test_operation common seq_w 223
 	#echo "开始测试普通时间序列乱续写入！"
 	#test_operation common unseq_w 223
-	echo "开始测试普通时间序列顺序写入！"
+	echo "开始测试普通时间序列顺序读写混合！"
 	test_operation common seq_rw 223
-	echo "开始测试普通时间序列乱续写入！"
+	echo "开始测试普通时间序列乱续读写混合！"
 	test_operation common unseq_rw 223
 	###############################对齐时间序列###############################
 	#echo "开始测试对齐时间序列顺序写入！"
 	#test_operation aligned seq_w 223
 	#echo "开始测试对齐时间序列乱续写入！"
 	#test_operation aligned unseq_w 223
-	echo "开始测试对齐时间序列顺序写入！"
+	echo "开始测试对齐时间序列顺序读写混合！"
 	test_operation aligned seq_rw 223
 	echo "开始测试对齐时间序列乱续写入！"
 	test_operation aligned unseq_rw 223
@@ -477,18 +477,18 @@ else
 	#test_operation template seq_w 223
 	#echo "开始测试模板时间序列乱续写入！"
 	#test_operation template unseq_w 223
-	#echo "开始测试模板时间序列顺序写入！"
+	#echo "开始测试模板时间序列顺序读写混合！"
 	#test_operation template seq_rw 223
-	#echo "开始测试模板时间序列乱续写入！"
+	#echo "开始测试模板时间序列乱续读写混合！"
 	#test_operation template unseq_rw 223
 	###############################对齐模板时间序列###############################
 	#echo "开始测试对齐模板时间序列顺序写入！"
 	#test_operation tempaligned seq_w 223
 	#echo "开始测试对齐模板时间序列乱续写入！"
 	#test_operation tempaligned unseq_w 223
-	#echo "开始测试对齐模板时间序列顺序写入！"
+	#echo "开始测试对齐模板时间序列顺序读写混合！"
 	#test_operation tempaligned seq_rw 223
-	#echo "开始测试对齐模板时间序列乱续写入！"
+	#echo "开始测试对齐模板时间序列乱续读写混合！"
 	#test_operation tempaligned unseq_rw 223
 	###############################测试完成###############################
 	echo "本轮测试${test_date_time}已结束."
