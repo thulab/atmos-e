8e5c331aad6339205cd81fd7819e805aa7fa7f65
冯庆新 <qingxin.feng@hotmail.com>
2023-08-24 16:55:50 +0800

    Update benchants.sh

--- script/benchants.sh
+++ script/benchants.sh
@@ -8,6 +8,7 @@ ATMOS_PATH=${INIT_PATH}/atmos-e
 BM_PATH=${INIT_PATH}/tsbs
 BUCKUP_PATH=/nasdata/repository/benchants
 REPOS_PATH=/nasdata/repository/master
+DATA_PATH=/root/dataset
 # nohup bin/tsbs_generate_data --use-case="devops" --seed=123 --scale=1000 --timestamp-start="2022-07-25T00:00:00Z" --timestamp-end="2022-07-28T00:00:00Z" --log-interval="10s" --format="iotdb" | gzip > iotdb-1000hosts-3days-10s.gz &
 # bin/tsbs_generate_queries --use-case="devops" --seed=123 --scale=1000 --timestamp-start="2022-07-25T00:00:00Z" --timestamp-end="2022-07-28T00:00:00Z" --queries=100000 --query-type="single-groupby-1-1-1" --format="iotdb"  > iotdb-query-single-groupby-1-1-1.txt
 #测试数据运行路径
@@ -21,7 +22,8 @@ protocol_class=(0 org.apache.iotdb.consensus.simple.SimpleConsensus org.apache.i
 Small_IP=172.20.70.12
 Medium_IP=172.20.70.10
 Control=172.20.70.6
-
+#query_list=(single-groupby-1-8-1 single-groupby-5-1-1 single-groupby-5-1-12 single-groupby-5-8-1 cpu-max-all-1 cpu-max-all-8 double-groupby-1 double-groupby-5 double-groupby-all high-cpu-all high-cpu-1 lastpoint groupby-orderby-limit)
+query_list=(single-groupby-1-8-1 single-groupby-5-1-1)
 ############mysql信息##########################
 MYSQLHOSTNAME="111.202.73.147" #数据库信息
 PORT="13306"
@@ -55,6 +57,7 @@ dataFileSize=0
 maxNumofOpenFiles=0
 maxNumofThread=0
 errorLogSize=0
+round=0
 ############定义监控采集项初始值##########################
 }
 local_ip=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
@@ -110,8 +113,8 @@ set_env() { # 拷贝编译好的iotdb到测试路径
 }
 modify_iotdb_config() { # iotdb调整内存，关闭合并
 	#修改IoTDB的配置
-	#sed -i "s/^#MAX_HEAP_SIZE=\"2G\".*$/MAX_HEAP_SIZE=\"20G\"/g" ${TEST_IOTDB_PATH}/conf/datanode-env.sh
-	#sed -i "s/^#MAX_HEAP_SIZE=\"2G\".*$/MAX_HEAP_SIZE=\"6G\"/g" ${TEST_IOTDB_PATH}/conf/confignode-env.sh
+	#sed -i "s/^#MAX_HEAP_SIZE=\"2G\".*$/MAX_HEAP_SIZE=\"10G\"/g" ${TEST_IOTDB_PATH}/conf/datanode-env.sh
+	#sed -i "s/^#MAX_HEAP_SIZE=\"2G\".*$/MAX_HEAP_SIZE=\"2G\"/g" ${TEST_IOTDB_PATH}/conf/confignode-env.sh
 	#关闭影响写入性能的其他功能
 	#sed -i "s/^# enable_seq_space_compaction=true.*$/enable_seq_space_compaction=false/g" ${TEST_IOTDB_PATH}/conf/iotdb-common.properties
 	#sed -i "s/^# enable_unseq_space_compaction=true.*$/enable_unseq_space_compaction=false/g" ${TEST_IOTDB_PATH}/conf/iotdb-common.properties
@@ -155,13 +158,53 @@ setup_env() {
 	echo "starting IoTDB ConfigNode on ${TEST_IP} ..."
 	pid3=$(ssh ${ACCOUNT}@${TEST_IP} "${TEST_IOTDB_PATH}/sbin/start-confignode.sh  > /dev/null 2>&1 &")
 	#主节点需要先启动，所以等待10秒是为了保证主节点启动完毕
-	sleep 10
+	sleep 5
 	#启动DataNode节点
 	echo "starting IoTDB DataNode on ${TEST_IP} ..."
 	pid3=$(ssh ${ACCOUNT}@${TEST_IP} "${TEST_IOTDB_PATH}/sbin/start-datanode.sh   > /dev/null 2>&1 &")
 	#等待60s，让服务器完成前期准备
+	sleep 10
+	for (( t_wait = 0; t_wait <= 50; t_wait++ ))
+	do
+	  str1=$(ssh ${ACCOUNT}@${TEST_IP} "${TEST_IOTDB_PATH}/sbin/start-cli.sh -h ${TEST_IP} -p 6667 -u root -pw root -e \"show cluster\" | grep 'Total line number = 2'")
+	  if [ "$str1" = "Total line number = 2" ]; then
+		echo "All Nodes is ready"
+		flag=1
+		break
+	  else
+		echo "All Nodes is not ready.Please wait ..."
+		sleep 3
+		continue
+	  fi
+	done
+	if [ "$flag" = "0" ]; then
+	  echo "All Nodes is not ready!"
+	  exit -1
+	fi
+}
+setup_env_q() {
+	TEST_IP=$1
+	echo "开始重置环境！"
+	ssh ${ACCOUNT}@${TEST_IP} "sudo reboot"
 	sleep 60
-	for (( t_wait = 0; t_wait <= 100; t_wait++ ))
+	echo "setting env to ${TEST_IP} ..."
+	#删除原有路径下所有
+	ssh ${ACCOUNT}@${TEST_IP} "rm -rf ${TEST_PATH}"
+	ssh ${ACCOUNT}@${TEST_IP} "mkdir -p ${TEST_PATH}"
+	#复制三项到客户机
+	scp -r ${TEST_PATH}/* ${ACCOUNT}@${TEST_IP}:${TEST_PATH}/
+	ssh ${ACCOUNT}@${TEST_IP} "cp -rf ${DATA_PATH}/data ${TEST_IOTDB_PATH}/"
+	#启动ConfigNode节点
+	echo "starting IoTDB ConfigNode on ${TEST_IP} ..."
+	pid3=$(ssh ${ACCOUNT}@${TEST_IP} "${TEST_IOTDB_PATH}/sbin/start-confignode.sh  > /dev/null 2>&1 &")
+	#主节点需要先启动，所以等待10秒是为了保证主节点启动完毕
+	sleep 5
+	#启动DataNode节点
+	echo "starting IoTDB DataNode on ${TEST_IP} ..."
+	pid3=$(ssh ${ACCOUNT}@${TEST_IP} "${TEST_IOTDB_PATH}/sbin/start-datanode.sh   > /dev/null 2>&1 &")
+	#等待60s，让服务器完成前期准备
+	sleep 10
+	for (( t_wait = 0; t_wait <= 50; t_wait++ ))
 	do
 	  str1=$(ssh ${ACCOUNT}@${TEST_IP} "${TEST_IOTDB_PATH}/sbin/start-cli.sh -h ${TEST_IP} -p 6667 -u root -pw root -e \"show cluster\" | grep 'Total line number = 2'")
 	  if [ "$str1" = "Total line number = 2" ]; then
@@ -268,10 +311,10 @@ collect_monitor_data() { # 收集iotdb数据大小，顺、乱序文件数量
 }
 backup_test_data() { # 备份测试数据
 	TEST_IP=$2
-	sudo mkdir -p ${BUCKUP_PATH}/$1/${commit_date_time}_${commit_id}_${protocol_class}
-	#str1=$(ssh ${ACCOUNT}@${TEST_IP} "rm -rf ${TEST_IOTDB_PATH}/data" 2>/dev/null)
-	scp -r ${ACCOUNT}@${TEST_IP}:${TEST_IOTDB_PATH}/ ${BUCKUP_PATH}/$1/${commit_date_time}_${commit_id}_${protocol_class}
-	sudo cp -rf ${BM_PATH}/TestResult/ ${BUCKUP_PATH}/$1/${commit_date_time}_${commit_id}_${protocol_class}
+	sudo mkdir -p ${BUCKUP_PATH}/$1/${commit_date_time}_${commit_id}_${protocol_class}/$3
+	str1=$(ssh ${ACCOUNT}@${TEST_IP} "rm -rf ${TEST_IOTDB_PATH}/data" 2>/dev/null)
+	scp -r ${ACCOUNT}@${TEST_IP}:${TEST_IOTDB_PATH}/ ${BUCKUP_PATH}/$1/${commit_date_time}_${commit_id}_${protocol_class}/$3
+	sudo cp -rf ${BM_PATH}/TestResult/ ${BUCKUP_PATH}/$1/${commit_date_time}_${commit_id}_${protocol_class}/$3
 }
 mv_config_file() { # 移动配置文件
 	rm -rf ${BM_PATH}/conf/config.properties
@@ -285,6 +328,18 @@ test_operation() {
 	#复制当前程序到执行位置
 	set_env
 	modify_iotdb_config
+	if [ "${server_kind}" = "medium" ]; then
+		#修改IoTDB的配置
+		sed -i "s/^#MAX_HEAP_SIZE=\"2G\".*$/MAX_HEAP_SIZE=\"10G\"/g" ${TEST_IOTDB_PATH}/conf/datanode-env.sh
+		sed -i "s/^#MAX_HEAP_SIZE=\"2G\".*$/MAX_HEAP_SIZE=\"2G\"/g" ${TEST_IOTDB_PATH}/conf/confignode-env.sh
+    elif [ "${server_kind}" = "small" ]; then
+        #修改IoTDB的配置
+		sed -i "s/^#MAX_HEAP_SIZE=\"2G\".*$/MAX_HEAP_SIZE=\"4G\"/g" ${TEST_IOTDB_PATH}/conf/datanode-env.sh
+		sed -i "s/^#MAX_HEAP_SIZE=\"2G\".*$/MAX_HEAP_SIZE=\"2G\"/g" ${TEST_IOTDB_PATH}/conf/confignode-env.sh
+	else
+		echo "服务器类型错误！"
+		return
+	fi
 	if [ "${protocol_class}" = "111" ]; then
 		set_protocol_class 1 1 1
 	elif [ "${protocol_class}" = "222" ]; then
@@ -298,8 +353,7 @@ test_operation() {
 		return
 	fi
 	#设置环境并启动IoTDB
-	setup_env ${TEST_IP}
-		
+	setup_env ${TEST_IP}	
 	echo "测试开始！"
 	rm -rf ${BM_PATH}/TestResult/*
 	echo "写入测试开始！"
@@ -318,44 +372,98 @@ test_operation() {
 	insert_sql="insert into ${TABLENAME} (commit_date_time,test_date_time,commit_id,author,server_kind,throughput_metrics,throughput_rows,query_rate,MIN_NUM,MEAN_NUM,MED_NUM,MAX_NUM,STDDEV_NUM,SUM_NUM,COUNT_NUM,numOfSe0Level,numOfUnse0Level,start_time,end_time,cost_time,dataFileSize,maxNumofOpenFiles,maxNumofThread,errorLogSize,remark) values(${commit_date_time},${test_date_time},'${commit_id}','${author}','${server_kind}',${throughput_metrics},${throughput_rows},${query_rate},${MIN_NUM},${MEAN_NUM},${MED_NUM},${MAX_NUM},${STDDEV_NUM},${SUM_NUM},${COUNT_NUM},${numOfSe0Level},${numOfUnse0Level},'${start_time}','${end_time}',${cost_time},${dataFileSize},${maxNumofOpenFiles},${maxNumofThread},${errorLogSize},'write')"
 	echo "${insert_sql}"
 	mysql -h${MYSQLHOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${insert_sql}"
+	backup_test_data ${server_kind} ${TEST_IP} write
+}
+test_operation_q() {
+	server_kind=$1
+	TEST_IP=$2
+	protocol_class=$3
+	echo "开始测试${server_kind}机型！"
+	#复制当前程序到执行位置
+	set_env
+	modify_iotdb_config
+	if [ "${server_kind}" = "medium" ]; then
+		#修改IoTDB的配置
+		sed -i "s/^#MAX_HEAP_SIZE=\"2G\".*$/MAX_HEAP_SIZE=\"10G\"/g" ${TEST_IOTDB_PATH}/conf/datanode-env.sh
+		sed -i "s/^#MAX_HEAP_SIZE=\"2G\".*$/MAX_HEAP_SIZE=\"2G\"/g" ${TEST_IOTDB_PATH}/conf/confignode-env.sh
+		#关闭影响写入性能的其他功能
+		sed -i "s/^# enable_seq_space_compaction=true.*$/enable_seq_space_compaction=false/g" ${TEST_IOTDB_PATH}/conf/iotdb-common.properties
+		sed -i "s/^# enable_unseq_space_compaction=true.*$/enable_unseq_space_compaction=false/g" ${TEST_IOTDB_PATH}/conf/iotdb-common.properties
+		sed -i "s/^# enable_cross_space_compaction=true.*$/enable_cross_space_compaction=false/g" ${TEST_IOTDB_PATH}/conf/iotdb-common.properties
+    elif [ "${server_kind}" = "small" ]; then
+        #修改IoTDB的配置
+		sed -i "s/^#MAX_HEAP_SIZE=\"2G\".*$/MAX_HEAP_SIZE=\"4G\"/g" ${TEST_IOTDB_PATH}/conf/datanode-env.sh
+		sed -i "s/^#MAX_HEAP_SIZE=\"2G\".*$/MAX_HEAP_SIZE=\"2G\"/g" ${TEST_IOTDB_PATH}/conf/confignode-env.sh
+		#关闭影响写入性能的其他功能
+		sed -i "s/^# enable_seq_space_compaction=true.*$/enable_seq_space_compaction=false/g" ${TEST_IOTDB_PATH}/conf/iotdb-common.properties
+		sed -i "s/^# enable_unseq_space_compaction=true.*$/enable_unseq_space_compaction=false/g" ${TEST_IOTDB_PATH}/conf/iotdb-common.properties
+		sed -i "s/^# enable_cross_space_compaction=true.*$/enable_cross_space_compaction=false/g" ${TEST_IOTDB_PATH}/conf/iotdb-common.properties
+	else
+		echo "服务器类型错误！"
+		return
+	fi
+	if [ "${protocol_class}" = "111" ]; then
+		set_protocol_class 1 1 1
+	elif [ "${protocol_class}" = "222" ]; then
+		set_protocol_class 2 2 2
+	elif [ "${protocol_class}" = "223" ]; then
+		set_protocol_class 2 2 3
+    elif [ "${protocol_class}" = "211" ]; then
+        set_protocol_class 2 1 1
+	else
+		echo "协议设置错误！"
+		return
+	fi
 	
-	throughput_metrics=0
-	throughput_rows=0
-	query_rate=0
-	MIN_NUM=0
-	MED_NUM=0
-	MEAN_NUM=0
-	MAX_NUM=0
-	STDDEV_NUM=0
-	SUM_NUM=0 
-	COUNT_NUM=0
-	numOfSe0Level=0
-	numOfUnse0Level=0
-	start_time=0
-	end_time=0
-	cost_time=0
-	dataFileSize=0
-	maxNumofOpenFiles=0
-	maxNumofThread=0
-	errorLogSize=0
-	
-	echo "查询测试开始！"
-	start_time=`date -d today +"%Y-%m-%d %H:%M:%S"`
-	nohup cat ${BM_PATH}/iotdb-query-single-groupby-1-1-1.txt | ${BM_PATH}/bin/tsbs_run_queries_iotdb --host="${TEST_IP}" --port="6667" --user="root" --password="root" --workers=100 --print-responses=false > ${BM_PATH}/TestResult/query_output.log 2>&1 &
-	#等待1分钟
-	sleep 60
-	monitor_test_status ${TEST_IP}
-	#测试结果收集写入数据库
-	#收集启动后基础监控数据
-	collect_monitor_data ${TEST_IP}
-	Outputfile=${BM_PATH}/TestResult/query_output.log
-	read query_rate <<<$(cat ${Outputfile} | grep "complete"| sed -n '1,1p' | awk '{print $12}')
-	read MIN_NUM MED_NUM MEAN_NUM MAX_NUM STDDEV_NUM SUM_NUM COUNT_NUM <<<$(cat ${Outputfile} | grep "min"| sed 's/ms//g' | sed 's/sec//g' | sed 's/,//g' | sed '$!d' | awk '{print $2,$4,$6,$8,$10,$12,$14}')
-	insert_sql="insert into ${TABLENAME} (commit_date_time,test_date_time,commit_id,author,server_kind,throughput_metrics,throughput_rows,query_rate,MIN_NUM,MEAN_NUM,MED_NUM,MAX_NUM,STDDEV_NUM,SUM_NUM,COUNT_NUM,numOfSe0Level,numOfUnse0Level,start_time,end_time,cost_time,dataFileSize,maxNumofOpenFiles,maxNumofThread,errorLogSize,remark) values(${commit_date_time},${test_date_time},'${commit_id}','${author}','${server_kind}',${throughput_metrics},${throughput_rows},${query_rate},${MIN_NUM},${MEAN_NUM},${MED_NUM},${MAX_NUM},${STDDEV_NUM},${SUM_NUM},${COUNT_NUM},${numOfSe0Level},${numOfUnse0Level},'${start_time}','${end_time}',${cost_time},${dataFileSize},${maxNumofOpenFiles},${maxNumofThread},${errorLogSize},'query')"
-	echo "${insert_sql}"
-	mysql -h${MYSQLHOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${insert_sql}"
-	
-	backup_test_data ${server_kind} ${TEST_IP}
+	echo "测试开始！"
+	rm -rf ${BM_PATH}/TestResult/*
+	for (( i = 0; i < ${#query_list[*]}; i++ ))
+	do
+		#设置环境并启动IoTDB
+		setup_env_q ${TEST_IP}
+		for (( j = 1; j <= 2; j++ ))
+		do
+			echo "开始${query_list[${i}]}查询的第${j}次测试！"
+			throughput_metrics=0
+			throughput_rows=0
+			query_rate=0
+			MIN_NUM=0
+			MED_NUM=0
+			MEAN_NUM=0
+			MAX_NUM=0
+			STDDEV_NUM=0
+			SUM_NUM=0 
+			COUNT_NUM=0
+			numOfSe0Level=0
+			numOfUnse0Level=0
+			start_time=0
+			end_time=0
+			cost_time=0
+			dataFileSize=0
+			maxNumofOpenFiles=0
+			maxNumofThread=0
+			errorLogSize=0
+			round=${j}
+			echo "查询测试开始！"
+			start_time=`date -d today +"%Y-%m-%d %H:%M:%S"`
+			nohup cat ${BM_PATH}/iotdb-query-${query_list[${i}]}.txt | ${BM_PATH}/bin/tsbs_run_queries_iotdb --host="${TEST_IP}" --port="6667" --user="root" --password="root" --workers=100 --print-responses=false > ${BM_PATH}/TestResult/query_output_${query_list[${i}]}.log 2>&1 &
+			#等待1分钟
+			sleep 10
+			monitor_test_status ${TEST_IP}
+			#测试结果收集写入数据库
+			#收集启动后基础监控数据
+			collect_monitor_data ${TEST_IP}
+			Outputfile=${BM_PATH}/TestResult/query_output_${query_list[${i}]}.log
+			read query_rate <<<$(cat ${Outputfile} | grep "complete"| sed -n '1,1p' | awk '{print $12}')
+			read MIN_NUM MED_NUM MEAN_NUM MAX_NUM STDDEV_NUM SUM_NUM COUNT_NUM <<<$(cat ${Outputfile} | grep "min"| sed 's/ms//g' | sed 's/sec//g' | sed 's/,//g' | sed '$!d' | awk '{print $2,$4,$6,$8,$10,$12,$14}')
+			insert_sql="insert into ${TABLENAME} (commit_date_time,test_date_time,commit_id,author,server_kind,throughput_metrics,throughput_rows,query_rate,MIN_NUM,MEAN_NUM,MED_NUM,MAX_NUM,STDDEV_NUM,SUM_NUM,COUNT_NUM,numOfSe0Level,numOfUnse0Level,start_time,end_time,cost_time,dataFileSize,maxNumofOpenFiles,maxNumofThread,errorLogSize,remark,round) values(${commit_date_time},${test_date_time},'${commit_id}','${author}','${server_kind}',${throughput_metrics},${throughput_rows},${query_rate},${MIN_NUM},${MEAN_NUM},${MED_NUM},${MAX_NUM},${STDDEV_NUM},${SUM_NUM},${COUNT_NUM},${numOfSe0Level},${numOfUnse0Level},'${start_time}','${end_time}',${cost_time},${dataFileSize},${maxNumofOpenFiles},${maxNumofThread},${errorLogSize},'${query_list[${i}]}',${j})"
+			echo "${insert_sql}"
+			mysql -h${MYSQLHOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${insert_sql}"
+			sleep 20
+		done		
+	done
+	#query没有存下日志（只有最后一次）
+	backup_test_data ${server_kind} ${TEST_IP} query
 }
 
 ##准备开始测试
@@ -371,9 +479,7 @@ else
 	update_sql="update ${TASK_TABLENAME} set ${test_type} = 'ontesting' where commit_id = '${commit_id}'"
 	result_string=$(mysql -h${MYSQLHOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${update_sql}")
 	echo "当前版本${commit_id}未执行过测试，即将编译后启动"
-	init_items
-	test_date_time=`date +%Y%m%d%H%M%S`
-	###############################普通时间序列###############################
+	###############################写入###############################
 	init_items
 	test_date_time=`date +%Y%m%d%H%M%S`
 	echo "开始测试中等配置！"
@@ -382,6 +488,15 @@ else
 	test_date_time=`date +%Y%m%d%H%M%S`
 	echo "开始测试小型配置！"
 	test_operation small ${Small_IP} 223
+	###############################查询###############################
+	init_items
+	test_date_time=`date +%Y%m%d%H%M%S`
+	echo "开始测试中等配置！"
+	test_operation_q medium ${Medium_IP} 223
+	init_items
+	test_date_time=`date +%Y%m%d%H%M%S`
+	echo "开始测试小型配置！"
+	test_operation_q small ${Small_IP} 223
 	###############################测试完成###############################
 	echo "本轮测试${test_date_time}已结束."
 	update_sql="update ${TASK_TABLENAME} set ${test_type} = 'done' where commit_id = '${commit_id}'"
