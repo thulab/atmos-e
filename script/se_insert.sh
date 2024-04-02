a6d9784e9112659ecdf765a9e87a000c902c6b8a
FLOW4215 <qingxin.feng@hotmail.com>
2024-04-02 14:12:22 +0800

    Update se_insert.sh

--- script/se_insert.sh
+++ script/se_insert.sh
@@ -232,11 +232,6 @@ collect_monitor_data() { # 收集iotdb数据大小，顺、乱序文件数量
 	walFileSize=$(get_single_index "max_over_time(file_size{instance=~\"${TEST_IP}:9091\",name=~\"wal\"}[$((m_end_time-m_start_time))s])" $m_end_time)
 	walFileSize=`awk 'BEGIN{printf "%.2f\n",'$walFileSize'/'1048576'}'`
 	walFileSize=`awk 'BEGIN{printf "%.2f\n",'$walFileSize'/'1024'}'`
-	echo $dataFileSize
-	echo $numOfSe0Level
-	echo $numOfUnse0Level
-	echo $maxNumofThread
-	echo $walFileSize
 }
 backup_test_data() { # 备份测试数据
 	sudo mkdir -p ${BUCKUP_PATH}/$1/${commit_date_time}_${commit_id}_${protocol_class}
@@ -319,10 +314,6 @@ test_operation() {
 
 	#停止IoTDB程序和监控程序
 	pid=$(${TEST_IOTDB_PATH}/sbin/start-cli.sh -h 127.0.0.1 -p 6667 -u root -pw root -e "flush")
-	stop_iotdb
-	sleep 30
-	check_benchmark_pid
-	check_iotdb_pid
 
 	#收集启动后基础监控数据
 	collect_monitor_data ${TEST_IP}
@@ -330,12 +321,17 @@ test_operation() {
 	csvOutputfile=${BM_PATH}/data/csvOutput/*result.csv
 	read okOperation okPoint failOperation failPoint throughput <<<$(cat ${csvOutputfile} | grep ^INGESTION | sed -n '1,1p' | awk -F, '{print $2,$3,$4,$5,$6}')
 	read Latency MIN P10 P25 MEDIAN P75 P90 P95 P99 P999 MAX <<<$(cat ${csvOutputfile} | grep ^INGESTION | sed -n '2,2p' | awk -F, '{print $2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12}')
-
+	
 	cost_time=$(($(date +%s -d "${end_time}") - $(date +%s -d "${start_time}")))
 	insert_sql="insert into ${TABLENAME} (commit_date_time,test_date_time,commit_id,author,ts_type,okPoint,okOperation,failPoint,failOperation,throughput,Latency,MIN,P10,P25,MEDIAN,P75,P90,P95,P99,P999,MAX,numOfSe0Level,start_time,end_time,cost_time,numOfUnse0Level,dataFileSize,maxNumofOpenFiles,maxNumofThread,errorLogSize,walFileSize,remark) values(${commit_date_time},${test_date_time},'${commit_id}','${author}','${ts_type}',${okPoint},${okOperation},${failPoint},${failOperation},${throughput},${Latency},${MIN},${P10},${P25},${MEDIAN},${P75},${P90},${P95},${P99},${P999},${MAX},${numOfSe0Level},'${start_time}','${end_time}',${cost_time},${numOfUnse0Level},${dataFileSize},${maxNumofOpenFiles},${maxNumofThread},${errorLogSize},${walFileSize},${protocol_class})"
-	echo ${insert_sql}
 
 	mysql -h${MYSQLHOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${insert_sql}"
+	
+	#停止IoTDB程序和监控程序
+	stop_iotdb
+	sleep 30
+	check_benchmark_pid
+	check_iotdb_pid
 
 	#备份本次测试
 	backup_test_data ${ts_type}
