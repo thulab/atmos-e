1be4f169e55a7fb98f4afcc610a5a2cac201c967
冯庆新 <qingxin.feng@hotmail.com>
2025-01-21 19:14:27 +0800

    准备切换测试环境
    对齐测试任务，添加异构网盘

--- tools/compile.sh
+++ tools/compile.sh
@@ -6,7 +6,7 @@ INIT_PATH=/home/atmos/zk_test
 IOTDB_PATH=${INIT_PATH}/timechodb
 FILENAME=${INIT_PATH}/gitlog.txt
 REPO_PATH=/nasdata/repository/master
-REPO_PATH_BK=/newnasdata/repository/master
+REPO_PATH_EX=/ex_nasdata/repository/master
 filter_list_folder_name=(client-cpp client-go client-py code-coverage compile-tools distribution docker docs example external-api external-pipe-api flink-iotdb-connector flink-tsfile-connector grafana-connector grafana-plugin hadoop hive-connector influxdb-protocol integration integration-test isession licenses mlnode openapi pipe-api rewrite-tsfile-tool schema-engine-rocksdb schema-engine-tag site spark-iotdb-connector spark-tsfile subscription-api test testcontainer tools trigger-api udf-api zeppelin-interpreter)
 
 ############mysql信息##########################
@@ -28,13 +28,24 @@ se_insert=0
 unse_insert=0
 se_query=0
 unse_query=0
-commpaction=0
+compaction=0
 sql_coverage=0
-api_insert=0
 weeklytest_insert=0
 weeklytest_query=0
+api_insert=0
+ts_performance=0
+cluster_insert=0
+cluster_insert_2=0
+insert_records=0
+restart_db=0
 routine_test=0
 config_insert=0
+count_ts=0
+pipe_test=0
+last_cache_query=0
+windows_test=0
+benchants=0
+helishi_test=0
 remark=0
 }
 sendEmail() {
@@ -61,7 +72,6 @@ sendEmail() {
 	esac
 	curl 'https://oapi.dingtalk.com/robot/send?access_token=f2d691d45da9a0307af8bbd853e90d0785dbaa3a3b0219dd2816882e19859e62' -H 'Content-Type: application/json' -d '{"msgtype": "text","text": {"content": "[Atmos]'${msgbody}'"}}' > /dev/null 2>&1 &
 }
-#启动后无限循环执行-之后加入crontab之后可以去掉该层循环
 for (( comp_test = 1; comp_test <= 3;))
 do
 	# 获取git commit对比判定是否启动测试
@@ -74,8 +84,8 @@ do
 	do
 		query_sql="select commit_id from ${TABLENAME} where commit_id='${commit_id_list[$i]}'"
 		echo "$query_sql"
-		abc=$(mysql -h${MYSQLHOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${query_sql}" | sed -n '2p')
-		if [ "${abc}" = "" ]; then
+		diff_str=$(mysql -h${MYSQLHOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${query_sql}" | sed -n '2p')
+		if [ "${diff_str}" = "" ]; then
 			cd ${IOTDB_PATH}
 			git_pull=$(timeout 100s git fetch --all)
 			git_pull=$(timeout 100s git reset --hard origin/master)
@@ -84,29 +94,25 @@ do
 			# 获取更新后git commit对比判定是否启动测试
 			commit_id=$(git log --pretty=format:"%h" -1 | cut -c1-7)
 			author=$(git log --pretty=format:"%an" -1)
-			#commit_id=${commit_id_list[$i]}
 			commit_date_time=$(git log --pretty=format:"%ai" -1 | cut -b 1-19 | sed s/-//g | sed s/://g | sed s/[[:space:]]//g)
 			#对比判定是否启动测试
 			echo "当前版本${commit_id}未记录,即将编译。"
 			#代码编译
+			date_time=`date +%Y%m%d%H%M%S`
 			comp_mvn=$(mvn clean package -pl distribution -am -DskipTests)
 			if [ $? -eq 0 ]
 			then
-				echo "编译完成！"
-				test_date_time=`date +%Y%m%d%H%M%S`
+				echo "${commit_id}编译完成！"
+				rm -rf ${REPO_PATH}/${commit_id}
 				mkdir -p ${REPO_PATH}/${commit_id}/apache-iotdb/
-				echo $commit_date_time
-				echo $test_date_time
 				cp -rf ${IOTDB_PATH}/distribution/target/timechodb-*-bin/timechodb-*-bin/* ${REPO_PATH}/${commit_id}/apache-iotdb/
 				#配置文件整理
 				#rm -rf ${REPO_PATH}/${commit_id}/apache-iotdb/conf/iotdb-system.properties
 				#mv ${REPO_PATH}/${commit_id}/apache-iotdb/conf/iotdb-system.properties.template ${REPO_PATH}/${commit_id}/apache-iotdb/conf/iotdb-system.properties
-				#向新的网盘环境复制一份备份
-				mkdir -p ${REPO_PATH_BK}/${commit_id}/apache-iotdb/
-				cp -rf ${IOTDB_PATH}/distribution/target/timechodb-*-bin/timechodb-*-bin/* ${REPO_PATH_BK}/${commit_id}/apache-iotdb/
-				#配置文件整理
-				#rm -rf ${REPO_PATH_BK}/${commit_id}/apache-iotdb/conf/iotdb-system.properties
-				#mv ${REPO_PATH_BK}/${commit_id}/apache-iotdb/conf/iotdb-system.properties.template ${REPO_PATH_BK}/${commit_id}/apache-iotdb/conf/iotdb-system.properties
+				#向异构机器网盘环境复制一份备份
+				rm -rf ${REPO_PATH_EX}/${commit_id}
+				mkdir -p ${REPO_PATH_EX}/${commit_id}/apache-iotdb/
+				cp -rf ${IOTDB_PATH}/distribution/target/timechodb-*-bin/timechodb-*-bin/* ${REPO_PATH}/${commit_id}/apache-iotdb/
 				#获取本次更新的变更文件列表
 				git log -1 --name-only > $FILENAME
 				#按照文件夹名称排除不必要测试文件夹
@@ -128,7 +134,7 @@ do
 				if [ "${file_num}" = "0" ]; then
 					#不需要测试
 					str_noneed='NoNeed'
-					insert_sql="insert into ${TABLENAME} (commit_date_time,commit_id,author,se_insert,unse_insert,se_query,unse_query,compaction,sql_coverage,weeklytest_insert,weeklytest_query,api_insert,routine_test,config_insert,ts_performance,cluster_insert,cluster_insert_2,cluster_insert_dt,benchants,pipe_test,windows_test,helishi_test) values(${commit_date_time},'${commit_id}','${author}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}')"
+					insert_sql="insert into ${TABLENAME} (commit_date_time,commit_id,author,se_insert,unse_insert,se_query,unse_query,compaction,sql_coverage,weeklytest_insert,weeklytest_query,api_insert,ts_performance,cluster_insert,cluster_insert_2,insert_records,restart_db,routine_test,config_insert,count_ts,pipe_test,last_cache_query,windows_test,benchants,helishi_test,remark) values(${commit_date_time},'${commit_id}','${author}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}',,'${str_noneed}''${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}','${str_noneed}')"
 					mysql -h${MYSQLHOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${insert_sql}"
 				else
 					#正常下派所有任务
@@ -137,9 +143,9 @@ do
 					echo "${commit_id}测试任务已发布！"
 				fi
 			else
-				echo "编译失败！"
+				echo "${commit_id}编译失败！"
 				str_err='CError'
-				insert_sql="insert into ${TABLENAME} (commit_date_time,commit_id,author,se_insert,unse_insert,se_query,unse_query,compaction,sql_coverage,weeklytest_insert,weeklytest_query,api_insert,routine_test,config_insert,ts_performance,cluster_insert,cluster_insert_2,cluster_insert_dt,benchants,pipe_test,windows_test,helishi_test) values(${commit_date_time},'${commit_id}','${author}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}')"
+				insert_sql="insert into ${TABLENAME} (commit_date_time,commit_id,author,se_insert,unse_insert,se_query,unse_query,compaction,sql_coverage,weeklytest_insert,weeklytest_query,api_insert,ts_performance,cluster_insert,cluster_insert_2,insert_records,restart_db,routine_test,config_insert,count_ts,pipe_test,last_cache_query,windows_test,benchants,helishi_test,remark) values(${commit_date_time},'${commit_id}','${author}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}',,'${str_err}''${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}','${str_err}')"
                 mysql -h${MYSQLHOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${insert_sql}"
 				sendEmail 2
 			fi
