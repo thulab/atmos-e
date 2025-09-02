03701f44f7d91b84ebd0dbde9050c370b117676c
冯庆新 <qingxin.feng@hotmail.com>
2025-09-02 08:44:25 +0800

    增加BM日志备份

--- script/cluster_insert.sh
+++ script/cluster_insert.sh
@@ -499,6 +499,7 @@ test_operation() {
 	done
 	sleep 10
 	sudo cp -rf ${BM_PATH}/TestResult/csvOutput/* ${BUCKUP_PATH}/${ts_type}/${commit_date_time}_${commit_id}_${data_type}_${protocol_class}/
+	sudo scp -r ${ACCOUNT}@${B_IP_list[1]}:${BM_PATH}/logs ${BUCKUP_PATH}/${ts_type}/${commit_date_time}_${commit_id}_${data_type}_${protocol_class}/
 }
 
 ##准备开始测试
